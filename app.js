d5b880cee8a3c7ebfc38a154e1fa8bacb5eb7a2b
philipwerner <philip.r.werner@gmail.com>
2017-07-11 15:14:27 -0700

    added first to questions to js, added border to css, modified html format to make easier to read

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-alert('hey is this thing on?')
+var username = prompt('What is your name?');
+
+alert('Hello ' + username + '. Lets play a quick game! Please answer Y/N to the following questions.');
+
+console.log('The user said their name is ' +username);
+
+var yoyoTricks = prompt('Can I do any sweet Yo-Yo tricks?');
+if (yoyoTricks == 'N') {
+  alert('That is correct, but I wish I could do sweet tricks');
+} else if (yoyoTricks == 'Y') {
+  alert('I am sorry but I am unable to do sweet tricks.');
+} else {
+  alert('Please answer Y or N. Please refresh page to try again');
+}
+console.log('The user answered ' +yoyoTricks);
+
+var stepMother = prompt('Do I have an evil Step Mother?');
+if (stepMother == 'N') {
+  alert('That is corect! I only have an awesome mother!');
+} else if (stepMother == 'Y') {
+  alert('Actually my mother is a sweetheart....');
+} else {
+  alert('Please enter Y or N. Refresh page to try again');
+}
+console.log('The user answered ' + stepMother);
