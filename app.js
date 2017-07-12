2265f2d69ecf6b58b3e6da4d6c6ebafa90dc5963
philipwerner <philip.r.werner@gmail.com>
2017-07-11 17:35:23 -0700

    fixed a issue with answers always getting the same response, still need to make multiple types of inputs accepted for y/n Y/N yes/no YES/NO

--- app.js
+++ app.js
@@ -6,51 +6,51 @@ alert('Hello ' + username + '. Lets play a quick game! Please answer Y/N to the 
 console.log('The user said their name is ' +username);
 
 var yoyoTricks = prompt('Can I do any sweet Yo-Yo tricks?');
-if (yoyoTricks == 'N') {
+if (yoyoTricks === 'N', 'NO') {
   alert('That is correct, but I wish I could do sweet tricks');
-} else if (yoyoTricks == 'Y') {
+} else if (yoyoTricks === 'Y') {
   alert('I am sorry but I am unable to do sweet tricks.');
 } else {
-  alert('Please answer Y or N. Please refresh page to try again');
+  alert('Please answer Y/N or YES/NO. Please refresh page to try again');
 }
-console.log('The user answered ' +yoyoTricks + ' to me being able to do sweet tricks.');
+console.log('The user answered ' +yoyoTricks + ' to me being able to do sweet yoyo tricks.');
 
 var stepMother = prompt('Do I have an evil Step Mother?');
-if (stepMother == 'N') {
+if (stepMother === 'N', 'NO') {
   alert('That is corect! I only have an awesome mother!');
-} else if (stepMother == 'Y') {
+} else if (stepMother === 'Y') {
   alert('Actually my mother is a sweetheart....');
 } else {
-  alert('Please enter Y or N. Refresh page to try again');
+  alert('Please answer Y/N or YES/NO. Please refresh page to try again');
 }
-console.log('The user answered ' + stepMother + ' to me having a evil stepmother');
+console.log('The user answered ' + stepMother + ' to me having a evil stepmother.');
 
 var drinkWater = prompt('Should I be drinking more water?');
-if (drinkWater == 'Y') {
+if (drinkWater === 'Y', 'YES') {
   alert('You are right, I really should drink more water.');
-} else if (drinkWater == 'N') {
+} else if (drinkWater === 'N') {
   alert('I like the way you think! But I probably should drink more water.');
 } else {
-  alert('Please enter Y or N. Refresh page to try again');
+  alert('Please answer Y/N or YES/NO. Please refresh page to try again');
 }
 console.log('The user answered ' + drinkWater + ' to me drinking more water');
 
 var harryPotter = prompt('Am I just a little to obsessed with the Harry Potter series?');
-if (harryPotter == 'N') {
+if (harryPotter === 'N', 'NO') {
   alert('Yeah, I was joking about getting that deathly hallows tattoo.....');
-} else if (harryPotter == 'Y') {
+} else if (harryPotter === 'Y') {
   alert('We should get deathly hallows tattoos together!');
 } else {
-  alert('Please enter Y or N. Refresh page to try again');
+  alert('Please answer Y/N or YES/NO. Please refresh page to try again');
 }
-console.log('The user answered ' + harryPotter + ' to a Harry Potter obsession.');
+console.log('The user answered ' + harryPotter + ' to me having a Harry Potter obsession.');
 
 var fireFly = prompt('Do I think the FireFly tv series is as good of a show as people say?');
-if (fireFly == 'Y') {
+if (fireFly === 'Y', 'YES') {
   alert('It is a pretty freaking awesome show and I wish they would bring it back.');
-} else if (fireFly == 'N') {
+} else if (fireFly === 'N') {
   alert('You are wrong, so so wrong.');
 } else {
-  alert('Please enter Y or N. Refresh page to try again');
+  alert('Please answer Y/N or YES/NO. Please refresh page to try again');
 }
 console.log('The user answered ' + fireFly + ' to me thinking FireFly is as good as people say');
