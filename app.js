99274cef20a39f04ea3e514b9fdae57e123eb4a3
philipwerner <philip.r.werner@gmail.com>
2017-07-12 14:11:04 -0700

    added logged correct answers from game and displayed correct answer total at end of quiz is js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var username = prompt('What is your name?');
 alert('Hello ' + username + '. Lets play a quick game! Please answer Y/N to the following questions.');
 
 console.log('The user said their name is ' +username);
+var correctAnswer = 0;
 
 var yoyoTricks = prompt('Can I do any sweet Yo-Yo tricks?').toUpperCase();
 if (yoyoTricks === 'N' || yoyoTricks ==='NO') {
@@ -15,6 +16,10 @@ if (yoyoTricks === 'N' || yoyoTricks ==='NO') {
 }
 console.log('The user answered ' +yoyoTricks + ' to me being able to do sweet tricks.');
 
+if (yoyoTricks === 'N' || yoyoTricks ==='NO') {
+  correctAnswer ++;
+}
+
 var stepMother = prompt('Do I have an evil Step Mother?').toUpperCase();
 if (stepMother === 'N' || stepMother === 'NO') {
   alert('That is corect! I only have an awesome mother!');
@@ -24,6 +29,10 @@ if (stepMother === 'N' || stepMother === 'NO') {
   alert('Please enter Y or N. Refresh page to try again');
 }
 console.log('The user answered ' + stepMother + ' to me having a evil stepmother');
+
+if (stepMother === 'N' || stepMother === 'NO') {
+  correctAnswer ++;
+}
 
 var drinkWater = prompt('Should I be drinking more water?').toUpperCase();
 if (drinkWater === 'Y' || drinkWater === 'YES') {
@@ -35,6 +44,10 @@ if (drinkWater === 'Y' || drinkWater === 'YES') {
 }
 console.log('The user answered ' + drinkWater + ' to me drinking more water');
 
+if (drinkWater === 'Y' || drinkWater === 'YES') {
+  correctAnswer ++;
+}
+
 var harryPotter = prompt('Am I just a little to obsessed with the Harry Potter series?').toUpperCase();
 if (harryPotter === 'N' || harryPotter === 'NO') {
   alert('Yeah, I was joking about getting that deathly hallows tattoo.....');
@@ -44,6 +57,10 @@ if (harryPotter === 'N' || harryPotter === 'NO') {
   alert('Please enter Y or N. Refresh page to try again');
 }
 console.log('The user answered ' + harryPotter + ' to a Harry Potter obsession.');
+
+if (harryPotter === 'Y' || harryPotter === 'YES') {
+  correctAnswer ++;
+}
 
 var fireFly = prompt('Do I think the FireFly tv series is as good of a show as people say?').toUpperCase();
 if (fireFly === 'Y' || fireFly === 'YES') {
@@ -55,7 +72,12 @@ if (fireFly === 'Y' || fireFly === 'YES') {
 }
 console.log('The user answered ' + fireFly + ' to me thinking FireFly is as good as people say');
 
+if (fireFly === 'Y' || fireFly === 'YES') {
+  correctAnswer ++;
+}
+
 var favGuess = 0;
+
 while(guessedNum != 16 && favGuess < 4) {
 var guessedNum = parseInt( prompt('Let\'s see if you can guess my favorite number.It is between 1 and 20. You have four tries and I will give you hints.'));
 if (guessedNum === 16) {
@@ -67,6 +89,14 @@ if (guessedNum === 16) {
 }
 favGuess++;
 }
-// } while(guessedNum != 16 || favGuess === 4);
-//
-// console.log(favGuess);
+
+if (guessedNum === 16) {
+  correctAnswer ++;
+}
+
+if (guessedNum != 16){
+  alert('Sorry about your luck ' + username + ', my favorite number is 16.');
+}
+
+
+alert(username + ' you correctly answered ' + correctAnswer + ' out of 7 questions correctly!!');
